a4b04d3ad980eae052d5fd4ada8e672200dc78ba
Pradeep Madubashena Gedara <44329815+madubashena89@users.noreply.github.com>
2018-12-02 20:33:16 -0600

    Add files via upload

---
+++ multiple_inheritance.py
@@ -0,0 +1,20 @@
+class OS:
+    name = "Android"
+    year = 2012
+    multitasking = True
+class Samsung :
+    website = "www.samsung.com"
+    Manufacture_country = "South Korea"
+
+
+class TV(Samsung):
+    def __init_(self):
+        print ("This is a product is made in %s. You can go to the website for more details %s" % (self.Manufacture_country, self.website))
+
+class S9(OS, Samsung):
+    def __init__(self):
+        if self.multitasking is True:
+            print ("THis is a multitasking system. Visit {} for more details".format(self.website))
+            print ("Name :" , self.name)
+S9_1 = S9()
+STV1 = TV()
